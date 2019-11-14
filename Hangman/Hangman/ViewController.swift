4b894bc739ff7f24ab80b9fd2d93153f4e591e40
Gregory Keeley <gregorykeeley@pursuit.org>
2019-11-14 17:23:06 -0500

    -Mostly functional project - single player mode from random word - 2 player coming soon...

--- Hangman/Hangman/ViewController.swift
+++ Hangman/Hangman/ViewController.swift
@@ -9,20 +9,69 @@
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
-
+//MARK: Outlets
     @IBOutlet weak var userInput: UITextField!
-    
     @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var beginGameButton: UIButton!
+    @IBOutlet weak var mainGameLabel: UILabel!
+    @IBOutlet weak var incorrectLettersLabel: UILabel!
+    @IBOutlet weak var incorrectGuessCounter: UILabel!
+    @IBOutlet weak var winOrLoseLabel: UILabel!
     
+    
+ //MARK: viewDidLoad
   override func viewDidLoad() {
     super.viewDidLoad()
     userInput.delegate = self
   }
-
-    @IBAction func inputBegin(_ sender: UITextField) {
-        hangmanBrain()
+    
+// MARK: Actions
+    
+    @IBAction func beginGameButtonPressed(_ sender: UIButton) {
+        beginGameButton.isEnabled = false
+        resetGame()
+        mainGameLabel.text = String(underscoresFromRandomWord)
+        incorrectLettersLabel.text = ("Tap \"Begin Game\" to start")
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        hangmanBrain(textField.text ?? "0")
+        mainGameLabel.text = String(underscoresFromRandomWord)
+        userInput.text = ""
+        incorrectLettersLabel.text = lettersGuessed.uppercased()
+        incorrectGuessCounter.text = String(guessCounter)
+         
+            if randomWord == (String(underscoresFromRandomWord)) {
+                winOrLoseLabel.text = "WINNER!!!"
+                beginGameButton.isEnabled = true
+                beginGameButton.setTitle("Tap here to play again", for: .normal)
+            } else if guessCounter > 5 {
+                winOrLoseLabel.text = "You lost..."
+                beginGameButton.setTitle("Tap here to play again", for: .normal)
+                mainGameLabel.text = randomWord
+            }
+        
+           switch guessCounter {
+            case 0:
+                mainImage.image = #imageLiteral(resourceName: "hang1")
+            case 1:
+                mainImage.image = #imageLiteral(resourceName: "hang2")
+            case 2:
+                mainImage.image = #imageLiteral(resourceName: "hang3")
+            case 3:
+                mainImage.image = #imageLiteral(resourceName: "hang4")
+            case 4:
+                mainImage.image = #imageLiteral(resourceName: "hang5")
+            case 5:
+                mainImage.image = #imageLiteral(resourceName: "hang6")
+            case 6:
+                mainImage.image = #imageLiteral(resourceName: "hang7")
+            default:
+                mainImage.image = #imageLiteral(resourceName: "hang1")
+            }
+        return userInput.resignFirstResponder()
+        
+    }
     
 }
 
--- Hangman/Hangman/hangmanBrain.swift
+++ Hangman/Hangman/hangmanBrain.swift
@@ -8,9 +8,10 @@
 
 import Foundation
 
-var randomWord = words.randomElement()?.lowercased() ?? "ERROR"
+var randomWord = words.randomElement()?.lowercased() ?? "Error"
+
 var underscoresFromRandomWord:[Character] = Array(String(repeating: "_", count: randomWord.count))
-var hiddenWord = String(underscoresFromRandomWord)
+    var hiddenWord = String(underscoresFromRandomWord)
 var maxGuesses = 5
 var guessCounter = 0
 var lettersGuessed = ""
@@ -20,107 +21,175 @@ var indices = [Int]()
 var totalGuesses = (0)
 var correctGuess = ""
 var winWord = ""
+
+var viewcontroller = ViewController()
+
+// MARK: Funcs
 func resetGame() -> () {
     randomWord = words.randomElement()?.lowercased() ?? "ERROR"
-    underscoresFromRandomWord = Array(String(repeating: "_", count: randomWord.count))
-    hiddenWord = String(underscoresFromRandomWord)
+    underscoresFromRandomWord = Array(String(repeating: "_ ", count: randomWord.count))
+    //hiddenWord = String(underscoresFromRandomWord)
     lettersGuessed = ""
     guessCounter = 0
-    repeatCondition = true
+    //repeatCondition = true
 }
 
 
-//=============================================================================================================
-// Hangman loop
-//=============================================================================================================
-func hangmanBrain() {
-    repeat {
+func hangmanBrain(_ userInput: String) {
+    if userInput.count > 1 || !alphabet.contains(userInput) || correctGuess.contains(userInput) || lettersGuessed.contains(userInput) {
+        print("please try again")
+    } else if randomWord.contains(userInput) {
+        for (index, char) in randomWord.enumerated() {
+            if userInput.contains(char) {
+                indices.append(index)
+                correctGuess.append(userInput)
+                winWord.append(userInput)
+            }
+        }
+        for (index, _) in underscoresFromRandomWord.enumerated() {
+            if indices.contains(index) {
+                underscoresFromRandomWord[index] = Character(userInput)
+            }
+        }
+        indices.removeAll()
+    }
+    if !randomWord.contains(userInput) {
+        guessCounter += 1
+        lettersGuessed.append(userInput + " ")
+        totalGuesses += 1
+        print(guessCounter)
+        print(randomWord)
         
-        if randomWord == (String(underscoresFromRandomWord)) {
-            let losePrompt = readLine()?.lowercased() ?? "no"
-            if losePrompt == "yes" {
-                resetGame()
-            } else {
-                break
-            }
-        }
-        print("Enter your character here:", terminator: " ")
-        let userInput = readLine()?.lowercased() ?? ""
-        if userInput.count > 1 || !alphabet.contains(userInput) || correctGuess.contains(userInput) || lettersGuessed.contains(userInput) {
-            sleep(1)
-            print("please try again")
-            print()
-            continue
-        }
-        if randomWord.contains(userInput) {
-            print()
-            for (index, char) in randomWord.enumerated() {
-                if userInput.contains(char) {
-                    indices.append(index)
-                    correctGuess.append(userInput)
-                    winWord.append(userInput)
-                }
-            }
-            for (index, _) in underscoresFromRandomWord.enumerated() {
-                if indices.contains(index) {
-                    underscoresFromRandomWord[index] = Character(userInput)
-                }
-            }
-            indices.removeAll()
-        }
-        if !randomWord.contains(userInput) {
-            guessCounter += 1
-            lettersGuessed.append(userInput + ", ")
-            totalGuesses += 1
-        }
-        print()
-        if guessCounter > maxGuesses {
-            print(hangManFinal)
-            print()
-            print("You lost! The man has been hung")
-            print()
-            print("It took you \(totalGuesses) guesses to complete")
-            print("The word was \"\(randomWord)\"")
-            print()
-            print("Would you like to play again?")
-            let losePrompt = readLine()?.lowercased() ?? "no"
-            if losePrompt == "yes" {
-                resetGame()
-            } else {
-                print("Goodbye!")
-                break
-            }
-        }
-        switch guessCounter {
-        case 0:
-            print(hangManEmpty)
-        case 1:
-            print()
-            print(hangMan1)
-        case 2:
-            print()
-            print(hangMan2)
-        case 3:
-            print()
-            print(hangMan3)
-        case 4:
-            print()
-            print(hangMan4)
-        case 5:
-            print()
-            print(hangMan5)
-        case 6:
-            print()
-            print(hangManFinal)
-        default:
-            print()
-        }
-        if randomWord.contains(userInput) {
-            print("You made a correct guess!")
-        } else {
-            print("Incorrect guess, try again")
-        }
-        print("Incorrect letters guessed: \(lettersGuessed)")
-        print("Current progress: \(String(underscoresFromRandomWord))")
-    } while repeatCondition
+    }
+
 }
+//func changeImage() {
+//    switch guessCounter {
+//    case 0:
+//        viewcontroller.mainImage.image = #imageLiteral(resourceName: "hang1")
+//    case 1:
+//        viewcontroller.mainImage.image = #imageLiteral(resourceName: "hang2")
+//    case 2:
+//        viewcontroller.mainImage.image = #imageLiteral(resourceName: "hang3")
+//    case 3:
+//        viewcontroller.mainImage.image = #imageLiteral(resourceName: "hang4")
+//    case 4:
+//        viewcontroller.mainImage.image = #imageLiteral(resourceName: "hang5")
+//    case 5:
+//        viewcontroller.mainImage.image = #imageLiteral(resourceName: "hang6")
+//    case 6:
+//        viewcontroller.mainImage.image = #imageLiteral(resourceName: "hang7")
+//    default:
+//        viewcontroller.mainImage.image = #imageLiteral(resourceName: "hang1")
+//    }
+//}
+
+//func winCheck() {
+//    if randomWord == (String(underscoresFromRandomWord)) {
+//        viewcontroller.winOrLoseLabel.text = "WINNER!!!"
+//        viewcontroller.beginGameButton.isEnabled = true
+//        viewcontroller.beginGameButton.setTitle("Tap here to play again", for: .normal)
+//    } else {
+//        viewcontroller.winOrLoseLabel.text = "You lost..."
+//        viewcontroller.beginGameButton.setTitle("Tap here to play again", for: .normal)
+//        viewcontroller.mainGameLabel.text = randomWord
+//    }
+//}
+
+
+
+
+
+
+
+//repeat {
+/*
+ if randomWord == (String(underscoresFromRandomWord)) {
+ let losePrompt = readLine()?.lowercased() ?? "no"
+ if losePrompt == "yes" {
+ resetGame()
+ } else {
+ //       break
+ }
+ }
+ print("Enter your character here:", terminator: " ")
+ 
+ /* MOVED TO hangmanBrain func
+ if userInput.count > 1 || !alphabet.contains(userInput) || correctGuess.contains(userInput) || lettersGuessed.contains(userInput) {
+ print("please try again")
+ }
+ */
+ 
+ if randomWord.contains(userInput) {
+ for (index, char) in randomWord.enumerated() {
+ if userInput.contains(char) {
+ indices.append(index)
+ correctGuess.append(userInput)
+ winWord.append(userInput)
+ }
+ }
+ for (index, _) in underscoresFromRandomWord.enumerated() {
+ if indices.contains(index) {
+ underscoresFromRandomWord[index] = Character(userInput)
+ }
+ }
+ indices.removeAll()
+ }
+ if !randomWord.contains(userInput) {
+ guessCounter += 1
+ lettersGuessed.append(userInput + ", ")
+ totalGuesses += 1
+ }
+ 
+ 
+ if guessCounter > maxGuesses {
+ print(hangManFinal)
+ print("You lost! The man has been hung")
+ print("It took you \(totalGuesses) guesses to complete")
+ print("The word was \"\(randomWord)\"")
+ print("Would you like to play again?")
+ let losePrompt = readLine()?.lowercased() ?? "no"
+ if losePrompt == "yes" {
+ resetGame()
+ } else {
+ print("Goodbye!")
+ //   break
+ }
+ }
+ 
+ switch guessCounter {
+ case 0:
+ print(hangManEmpty)
+ case 1:
+ 
+ print(hangMan1)
+ case 2:
+ 
+ print(hangMan2)
+ case 3:
+ 
+ print(hangMan3)
+ case 4:
+ 
+ print(hangMan4)
+ case 5:
+ 
+ print(hangMan5)
+ case 6:
+ 
+ print(hangManFinal)
+ default:
+ print()
+ }
+ 
+ 
+ if randomWord.contains(userInput) {
+ print("You made a correct guess!")
+ } else {
+ print("Incorrect guess, try again")
+ }
+ print("Incorrect letters guessed: \(lettersGuessed)")
+ print("Current progress: \(String(underscoresFromRandomWord))")
+ //    } while repeatCondition
+ }
+ */
